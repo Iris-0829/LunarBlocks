d4c8a43aff8f373ca484b8535795fab862df0838
Iris-0829 <1648333063@qq.com>
2020-10-16 19:27:19 -0400

    update CommandList

--- components/CommandList.py
+++ components/CommandList.py
@@ -1,4 +1,4 @@
-from typing import Tuple, List
+from typing import Tuple
 from components import operator
 
 
@@ -8,38 +8,48 @@ class Commands:
     components.Commands: Stores all commands in sequential order
     ------------------------------------------------------------------
     """
-    def __init__(self):
+
+    def __init__(self, ope: operator):
         """
         ------------------------------------------------------------------
         __init__: Initializes Commands with a list of commands and a test set
         ------------------------------------------------------------------
         """
-        self.commandList = []
+        self.command = ope
         self.testSet = []
 
     def addTestCase(self, testInput: Tuple[int, int], testOutput: int):
+        """
+        ------------------------------------------------------------------
+        addTestCase: Add test cases to testSet
+        ------------------------------------------------------------------
+        Parameters:
+            testInput: input of test
+            testOutput: expected output
+        Returns:
+            None
+        ------------------------------------------------------------------
+        """
         # testInput: (0,1)  testOutput: 1
         # self.testSet: [ [(0,1), 1], [(3,5), 8], [(10, 12), 22]]
         testCase = [testInput, testOutput]
         self.testSet.append(testCase)
 
-    def addCommand(self, ope: operator):
-        self.commandList.append(operator)
-
     def runTest(self):
+        """
+        ------------------------------------------------------------------
+        runTest: run the test cases in testSet
+        ------------------------------------------------------------------
+        Returns:
+            True if it passes all cases
+            False if there's at least one case fails
+        ------------------------------------------------------------------
+        """
         for test in self.testSet:
             # test: [(0,1), 1]
             # exp = 1
-            act = run(test[0])
+            act = self.command.doOperation(test[0][0], test[0][1])
             exp = test[1]
             if act != exp:
                 return False
         return True
-
-    def run(self, testInput: Tuple[int, int]):
-        # testInput: (0,1)
-        # output should 1
-
-
-
-
