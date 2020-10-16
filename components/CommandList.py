050839f59ae8be86217844accb8deffbc27c63e3
Iris-0829 <1648333063@qq.com>
2020-10-16 18:36:08 -0400

    update CommandList

--- components/CommandList.py
+++ components/CommandList.py
@@ -1,25 +1,45 @@
-class CommandList:
+from typing import Tuple, List
+from components import operator
+
+
+class Commands:
     """
     ------------------------------------------------------------------
-    components.CommandList: Stores all commands in sequential order
+    components.Commands: Stores all commands in sequential order
     ------------------------------------------------------------------
     """
     def __init__(self):
         """
         ------------------------------------------------------------------
-        __init__: Initializes CommandList with a list
+        __init__: Initializes Commands with a list of commands and a test set
         ------------------------------------------------------------------
         """
-        self.command_list = []
-        self.addCommand()
+        self.commandList = []
+        self.testSet = []
 
-    def addCommand(self, operator: object):
-        """
-        ------------------------------------------------------------------
-        addCommand: Add operators to command_list
-        ------------------------------------------------------------------
-        """
-        if operator not in self.command_list:
-            self.command_list.append(operator)
+    def addTestCase(self, testInput: Tuple[int, int], testOutput: int):
+        # testInput: (0,1)  testOutput: 1
+        # self.testSet: [ [(0,1), 1], [(3,5), 8], [(10, 12), 22]]
+        testCase = [testInput, testOutput]
+        self.testSet.append(testCase)
+
+    def addCommand(self, ope: operator):
+        self.commandList.append(operator)
+
+    def runTest(self):
+        for test in self.testSet:
+            # test: [(0,1), 1]
+            # exp = 1
+            act = run(test[0])
+            exp = test[1]
+            if act != exp:
+                return False
+        return True
+
+    def run(self, testInput: Tuple[int, int]):
+        # testInput: (0,1)
+        # output should 1
+
+
 
 
