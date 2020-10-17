f37a7f4f12379ff36aa924ba27164f0fe078d139
Jesse M <jesse.maltese@mail.utoronto.ca>
2020-10-17 02:43:35 -0400

    fixes

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import pygame
+import math
 from collections import defaultdict
-from components.Addition import * 
-from components.Subtraction import * 
+from components.Addition import *
+from components.Subtraction import *
 
 
+
+rad = math.pi/180
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-grid_square_size = 15 #15x15px  
+grid_square_size = 15  # 15x15px
 FPS = 30
 LINE_ID = 0
 SHAPE_ID = 0
@@ -16,7 +19,6 @@ operand_set = []
 shapes = defaultdict(list)
 lines = {}
 edges = defaultdict(list)
-sites = pygame.sprite.Group()
 
 
 pygame.init()
@@ -27,9 +29,13 @@ surface = pygame.Surface(screen.get_size()).convert()
 
 clock = pygame.time.Clock()
 
-#TODO: clean this up 
+# TODO: clean this up
+# It doesn't really need to be in this file.
+# I'll keep it around for now though. 
+
+
 def add_line(shape1, shape2):
-    #create line between shapes.
+    # create line between shapes.
     global SHAPE_ID
     global LINE_ID
     try:
@@ -40,13 +46,15 @@ def add_line(shape1, shape2):
         lines[LINE_ID] = [(shape1.x, shape1.y), (shape2.x, shape2.y)]
         LINE_ID += 1
     except:
-        print("addLine(), problem!")   
+        print("addLine(), problem!")
     return
+
 
 def add_shape(shape, shape_img, operator):
     global SHAPE_ID
     shapes[SHAPE_ID] = [shape, shape_img, operator]
     SHAPE_ID += 1
+
 
 def get_shape_id(shape):
     global SHAPE_ID
@@ -56,6 +64,7 @@ def get_shape_id(shape):
     shapes[SHAPE_ID] = shape[0]
     SHAPE_ID += 1
     return SHAPE_ID - 1
+
 
 def get_connected_lines(shape_id, shape_pos):
     d = {}
@@ -68,9 +77,21 @@ def get_connected_lines(shape_id, shape_pos):
     return d
 
 
-#=======
+# From https://stackoverflow.com/questions/56295712/how-to-draw-a-dynamic-arrow-in-pygame
+def arrow(screen, lcolor, tricolor, start, end, trirad, thickness=2):
+    pygame.draw.line(screen, lcolor, start, end, thickness)
+    rotation = (math.atan2(start[1] - end[1], end[0] - start[0])) + math.pi/2
+    pygame.draw.polygon(screen, tricolor, ((end[0] + trirad * math.sin(rotation),
+                                        end[1] + trirad * math.cos(rotation)),
+                                       (end[0] + trirad * math.sin(rotation - 120*rad),
+                                        end[1] + trirad * math.cos(rotation - 120*rad)),
+                                       (end[0] + trirad * math.sin(rotation + 120*rad),
+                                        end[1] + trirad * math.cos(rotation + 120*rad))))
+
+
+# =======
 # For Testing Purposes
-#======
+# ======
 for i in range(2):
     add = Addition(0, i*10, i*10)
     s = add.draw()
@@ -81,14 +102,13 @@ for i in range(2):
     add_shape(s[0], s[1], sub)
 
 
-
 def game_loop():
     dragging = False
     dragged = None
     dragged_init_pos = None
     dragged_id = -1
 
-    line_num = -1 
+    line_num = -1
 
     selected = None
     drawing = False
@@ -99,72 +119,72 @@ def game_loop():
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
-                    #if(button.collidepoint(event.pos)):
+                    # This is how we would create an "add/delete" button:
+                    # if(button.collidepoint(event.pos)):
                     #    operatorSet.append(pygame.Rect(200, 200, 30, 30))
-                    #else:
+                    # else:
                     if not dragging:
                         for shape_id in shapes:
                             shape = shapes[shape_id][0]
                             if(shape.collidepoint(event.pos)):
                                 if(selected != shape) and (selected is not None):
-                                    #draw line
+                                    # draws the line and deselect shape
                                     add_line(selected, shape)
                                     selected = None
-                                    shapes[dragged_id][1] = shapes[dragged_id][2].selected()[1]
+                                    shapes[dragged_id][1] = shapes[dragged_id][2].draw()[1]
                                     break
                                 else:
+                                    # Begins the dragging sequence.
                                     dragging = True
                                     dragged = shape
                                     dragged_id = get_shape_id(dragged)
                                     dragged_init_pos = (shape.x, shape.y)
-                                    connected_lines = get_connected_lines(dragged_id, dragged_init_pos)
+                                    # Gets all lines connected to shape, along with which end is connected to our shape.
+                                    connected_lines = get_connected_lines(
+                                        dragged_id, dragged_init_pos)
+
                                     m_x, m_y = event.pos
                                     offset_x = shape.x - m_x
                                     offset_y = shape.y - m_y
                                     break
-                                
-                    
+
             if event.type == pygame.MOUSEBUTTONUP:
                 if event.button == 1:
                     if(dragged is not None and dragged_init_pos is not None):
                         if(dragged.x == dragged_init_pos[0] and dragged.y == dragged_init_pos[1]):
                             selected = dragged
-                            #This is a convoluted way of making the shape into the "selected" version.
-                            shapes[dragged_id][1] = shapes[dragged_id][2].selected()[1]
-
+                            # This is a convoluted way of making the shape into the "selected" version.
+                            s = shapes[dragged_id]
+                            s[1] = s[2].selected()[1]
 
                     dragging = False
                     dragged = None
                     dragged_init_pos = None
 
-
             if event.type == pygame.MOUSEMOTION:
                 if dragging:
+                    #move shape and snap to grid
                     m_x, m_y = event.pos
-                    dragged.x = round((m_x + offset_x) / grid_square_size)*grid_square_size
-                    dragged.y = round((m_y + offset_y) / grid_square_size)*grid_square_size
-
+                    dragged.x = round((m_x + offset_x) /
+                                      grid_square_size)*grid_square_size
+                    dragged.y = round((m_y + offset_y) / 
+                                      grid_square_size)*grid_square_size
+                    
+                    #move our line properly
                     for key in connected_lines:
                         idx = connected_lines[key]
                         line = lines[key]
                         line[idx] = (dragged.x, dragged.y)
 
-
-
-            screen.fill((255,255,255))
+            screen.fill((255, 255, 255))
 
             for shape_id in shapes:
                 screen.blit(shapes[shape_id][1], shapes[shape_id][0])
-                #pygame.draw.rect(screen, (0,255,0), shape)
-            
-            for line in lines:
-                pygame.draw.line(screen, (0, 0, 0),
-                                 lines[line][0], lines[line][1], 4)
-            
+
+            for line in lines:  
+                arrow(screen, (0,0,0), (0,0,0), lines[line][0], lines[line][1], 10, 5)
+
             pygame.display.flip()
-
-
-
 
 
 game_loop()
