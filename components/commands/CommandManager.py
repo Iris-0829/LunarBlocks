5852c67933a66704ae84f91be3d4e6a9096718b7
Jan Garong <jan.garong@mail.utoronto.ca>
2020-10-17 19:54:20 -0400

    Add start and reset to CommandManager

--- components/commands/CommandManager.py
+++ components/commands/CommandManager.py
@@ -10,12 +10,32 @@ class CommandManager:
         :param ports: List of tuples in the form (operand, next_node).
         :param op_nodes: List of all known operator nodes in the stage.
         """
+        self.ports = ports
         self.in_node = InNode(ports)
         self.op_nodes = op_nodes
-        self.in_node.advance()
         self.out_node = out_node
 
+    def start(self):
+        """
+        Moves shapes in the input node into the operator nodes.
+        """
+        self.in_node.advance()
+
+    def reset(self):
+        """
+        Resets all nodes back to what it was.
+        """
+        self.in_node = InNode(self.ports)
+        self.out_node.params = []
+        for op in self.op_nodes:
+            op.params = []
+            op.passed = False
+            op.output = None
+
     def execute(self):
+        """
+        Executes an operation, if applicable.
+        """
         for op in self.op_nodes:
             # op.execute()
             res = op.execute()
@@ -24,5 +44,8 @@ class CommandManager:
                 op.advance()
                 break  # prevents displaying more than one I/O at a time.
 
-    def get_output(self):
-        self.out_node.execute()
+    def get_output(self) -> list:
+        """
+        Returns output as a list.
+        """
+        return self.out_node.execute()
--- test/TestCommandManager.py
+++ test/TestCommandManager.py
@@ -13,6 +13,12 @@ s3 = Shape(None, '', 3, (5, 5), (0, 0, 0))
 a = [a1, a2]
 ports = [(s1, a2), (s2, a2), (s3, a1)]
 cm = CommandManager(ports=ports, op_nodes=a, out_node=out)
+cm.start()
+cm.execute()
+cm.execute()
+print(out.execute()[0].scale)
+cm.reset()
+cm.start()
 cm.execute()
 cm.execute()
 print(out.execute()[0].scale)
